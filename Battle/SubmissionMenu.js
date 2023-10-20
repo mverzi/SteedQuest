5cec63ece0b0e871d812d2dc28b5223b0e3b2c06
mverzi <maggie.verzi@gmail.com>
2023-10-20 13:19:51 -0400

    Updates

--- Battle/SubmissionMenu.js
+++ Battle/SubmissionMenu.js
@@ -5,14 +5,90 @@ class SubmissionMenu {
         this.onComplete = onComplete;
     }
 
-    decide() {
+    getPages() {
+
+        const backOption = {
+            label: "Go Back",
+            description: "Return to previous page",
+            handler: () => {
+                this.keyboardMenu.setOptions(this.getPages().root);
+            }
+        }
+
+        return {
+            root: [
+                {
+                    label: "Attack",
+                    description: "Choose an attack",
+                    handler: () => {
+                        //go to attack page
+                        this.keyboardMenu.setOptions(this.getPages().attacks)
+                    }
+                },
+                {
+                    label: "Items",
+                    description: "Choose an item",
+                    handler: () => {
+                        //go to items page
+                        this.keyboardMenu.setOptions(this.getPages().items)
+                    }
+                },
+                {
+                    label: "Swap",
+                    description: "Choose another horse",
+                    handler: () => {
+                        //See horse options
+                    }
+                }
+
+            ],
+            attacks: [
+                ...this.caster.actions.map(key => {
+                    const action = Actions[key];
+                    return {
+                        label: action.name,
+                        description: action.description,
+                        handler: () => {
+                            this.menuSubmit(action);
+                        }
+                    }
+                }),
+                backOption
+            ],
+            items: [
+                // items go here
+                backOption
+            ]
+        }
+    }
+
+    menuSubmit(action, instanceId=null) {
+
+        this.keyboardMenu?.end();
+
         this.onComplete({
-            action: Actions[this.caster.actions[0]],
-            target: this.enemy
+            action,
+            target: action.targetType === "friendly" ? this.caster : this.enemy
         })
     }
 
+    decide() {
+        //to do - enemy deciding what move to use 
+        this.menuSubmit(Actions[this.caster.actions[0]]);
+    }
+
+    showMenu(container){
+        this.keyboardMenu = new KeyboardMenu();
+        this.keyboardMenu.init(container);
+        this.keyboardMenu.setOptions(this.getPages().root);
+    }
+
     init(container) {
-        this.decide();
+
+        if(this.caster.isPlayerControlled){
+            this.showMenu(container);
+        } else {
+            this.decide();
+        }
     }
 }
---
+++ KeyboardMenu.js
@@ -0,0 +1,86 @@
+class KeyboardMenu {
+    constructor() {
+        this.options = [];
+        this.up = null;
+        this.down = null;
+        this.prevFocus = null;
+    }
+
+    setOptions(options){
+        this.options = options;
+        this.element.innerHTML = this.options.map((option, index) => {
+            const disabledAttr = option.disabled ? "disabled" : "";
+            return (`
+            <div class="option">
+                <button ${disabledAttr} data-button="${index}" data-description="${option.description}">
+                    ${option.label}
+                </button>
+                <span class="right">${option.right ? option.right() : ""}</span>
+            </div>
+            `)
+        }).join("");
+
+        this.element.querySelectorAll("button").forEach(button => {
+            button.addEventListener("click", () => {
+                const chosenOption = this.options[Number(button.dataset.button)];
+                chosenOption.handler();
+            })
+            button.addEventListener("mouseenter", () => {
+                button.focus();
+            })
+            button.addEventListener("focus", () => {
+                this.prevFocus = button;
+                this.descriptionElementText.innerText = button.dataset.description;
+            })
+        })
+
+        setTimeout(() => {
+            this.element.querySelector("button[data-button]:not([disabled])").focus();
+        }, 10)
+
+    }
+
+    createElement() {
+        //create menu
+        this.element = document.createElement("div");
+        this.element.classList.add("KeyboardMenu");
+
+        //create description for each menu item
+        this.descriptionElement = document.createElement("div");
+        this.descriptionElement.classList.add("DescriptionBox");
+        this.descriptionElement.innerHTML = (`<p>Description coming soon</p>`);
+        this.descriptionElementText = this.descriptionElement.querySelector("p");
+    }
+
+    end() {
+        //Remove these from the DOM when no longer needed
+        this.element.remove();
+        this.descriptionElement.remove();
+
+        //make sure arrow keys behave correctly once menu is closed
+        this.up.unbind();
+        this.down.unbind();
+    }
+
+    init(container) {
+        this.createElement();
+        container.appendChild(this.descriptionElement);
+        container.appendChild(this.element);
+    
+        this.up = new KeyPressListener("ArrowUp", () => {
+          const current = Number(this.prevFocus.getAttribute("data-button"));
+          const prevButton = Array.from(this.element.querySelectorAll("button[data-button]")).reverse().find(el => {
+            return el.dataset.button < current && !el.disabled;
+          })
+          prevButton?.focus();
+        })
+        this.down = new KeyPressListener("ArrowDown", () => {
+          const current = Number(this.prevFocus.getAttribute("data-button"));
+          const nextButton = Array.from(this.element.querySelectorAll("button[data-button]")).find(el => {
+            return el.dataset.button > current && !el.disabled;
+          })
+          nextButton?.focus();
+        })
+    
+      }
+}
